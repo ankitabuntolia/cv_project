0bcfa9e851759fa24796ba6ebc6313683e7d8084
Michael <Michael>
2019-12-31 11:14:36 +0100

    replaced hough by geometry check

--- CV_lena.m
+++ CV_lena.m
@@ -20,7 +20,7 @@ rgbpath = './data/peaches/bottom/RGB';
 rgbds = datastore( rgbpath );
 
 all_centers_bottom = cell(length(rgbds.Files),1);
-for useId = 1:2%length(rgbds.Files)
+for useId = 1:length(rgbds.Files)
     all_centers_bottom{useId} = detect_peaches(useId, rgbds, cameraParams, true, 1);
 end
 
@@ -30,7 +30,7 @@ centers = all_centers_top{8};
 [worldPoints] = centers_to_world_points(useId, rgbds, cameraParams, centers);
 
 %% for testing projections draw world points of peaches from one img to another
-draw_peach_centers_in_img(useId+1, rgbds, cameraParams, worldPoints);
+draw_peach_centers_in_img(useId, rgbds, cameraParams, worldPoints);
 
 %% functions for my first tracking ideas
 % WARNING! something's off with calculation of worldPoints or the inverse (or both), 
@@ -124,34 +124,72 @@ function [centers] = detect_peaches(useId, rgbds, cameraParams, showResults, thr
         title('Results')
     end
 
-    %% WIP detection method 1: find connected pixel regions (centers, bounding boxes and area)
+    %% detection method 1: find connected pixel regions (centers, bounding boxes and area)
     peaches = regionprops(openBW, 'Centroid', 'BoundingBox', 'Area');
 
     %% WIP detection method 2: circular hough transform
-    [centers, radii, ~] = imfindcircles(openBW,[40*imgscale 120*imgscale]);
-    viscircles(centers, radii,'EdgeColor','b');
+%     [centers, radii, ~] = imfindcircles(openBW,[40*imgscale 120*imgscale]);
+%     viscircles(centers, radii,'EdgeColor','b');
 
     %% WIP detection method 3: circular hough transform with other parametrization
-    [centers, radii, ~] = imfindcircles(openBW,[24*imgscale 60*imgscale]);
-    viscircles(centers, radii,'EdgeColor','r');
+%     [centers, radii, ~] = imfindcircles(openBW,[24*imgscale 60*imgscale]);
+%     viscircles(centers, radii,'EdgeColor','r');
 
-    %% WIP combining info of 3 detection approaches
+    %% Hough transform detection doesn't improve results
+    %% new approach: check with geometric information of border boxes if peach was detected twice
+    measurements = peaches;
     
-    %% print peaches centers with bounding boxes
-    for k = 1 : length(peaches)
-        thisBB = peaches(k).BoundingBox;
-        rectangle('Position',[thisBB(1),thisBB(2),thisBB(3),thisBB(4)],'EdgeColor','b','LineWidth',1 );
-        plot( peaches(k).Centroid(1), peaches(k).Centroid(2), 'rx', 'MarkerSize', 10);
+    f = fieldnames(measurements)';
+    f{2,1} = {};
+    peaches = struct(f{:});
+    
+    idx = 1;
+    for k = 1 : length(measurements)
+        thisBB = measurements(k).BoundingBox;
+        addPeach = true;
+        
+        % look for bounding boxes of certain maximum size
+        if thisBB(3) <= 12 && thisBB(4) <= 12
+            % if bounding box is small enough, look for near centroid in
+            % already added data
+            for i = 1: length(measurements)
+                if k ~= i
+                    distanceX = abs(measurements(k).Centroid(1) - measurements(i).Centroid(1));
+                    distanceY = abs(measurements(k).Centroid(2) - measurements(i).Centroid(2));
+                    distance = sqrt(distanceX^2 + distanceY^2);
+                    % add only if no other center is far away enough
+                    if distance < 20
+                        addPeach = false;
+                    end
+                end
+            end
+        end
+        
+        if addPeach
+            peaches(idx) = measurements(k);
+            idx = idx+1;
+        end
     end
+    
+    if ~isempty(peaches)
+        %% print peaches centers with bounding boxes
+        for k = 1 : length(peaches)
+            thisBB = peaches(k).BoundingBox;
+            rectangle('Position',[thisBB(1),thisBB(2),thisBB(3),thisBB(4)],'EdgeColor','b','LineWidth',1 );
+            plot( peaches(k).Centroid(1), peaches(k).Centroid(2), 'rx', 'MarkerSize', 10);
+        end
 
-    %% put center coordinates (x,y) in separate cell array and rescale them for tracking with sfm
-    elements = struct2cell(peaches)';
-    centers_scaled = elements(:,2);
-    centers = zeros(length(centers_scaled),2);
+        %% put center coordinates (x,y) in separate cell array and rescale them for tracking with sfm
+        elements = struct2cell(peaches')';
+        centers_scaled = elements(:,2);
+        centers = zeros(length(centers_scaled),2);
 
-    for i = 1 : length(centers_scaled)
-        x_new = interp1([1 6000*imgscale], [1 6000], centers_scaled{i}(1,1));
-        y_new = interp1([1 4000*imgscale], [1 4000], centers_scaled{i}(1,2));
-        centers(i,:) = [x_new, y_new];
+        for i = 1 : length(centers_scaled)
+            x_new = interp1([1 6000*imgscale], [1 6000], centers_scaled{i}(1,1));
+            y_new = interp1([1 4000*imgscale], [1 4000], centers_scaled{i}(1,2));
+            centers(i,:) = [x_new, y_new];
+        end
+    else
+        centers = [];
     end
 end
