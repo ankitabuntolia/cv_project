110ee6606f877c475068f79cb638562aed0a0504
Magdalena Hader <38649052+haderlena@users.noreply.github.com>
2020-01-04 06:39:22 +0100

    commented out resizing and fixed values

--- src/detectCenters.m
+++ src/detectCenters.m
@@ -1,18 +1,8 @@
 function [peaches] = detectCenters(binary)
-  %% detection method 1: find connected pixel regions (centers, bounding boxes and area)
+  %% find connected pixel regions (centers, bounding boxes and area)
   peaches = regionprops(binary, 'Centroid', 'BoundingBox', 'Area');
 
-  %% WIP detection method 2: circular hough transform
-%  [centers, radii, ~] = imfindcircles(openBW,[40*imgscale 120*imgscale]);
-%  viscircles(centers, radii,'EdgeColor','b');
-
-  %% WIP detection method 3: circular hough transform with other parametrization
-%  [centers, radii, ~] = imfindcircles(openBW,[24*imgscale 60*imgscale]);
-%  iscircles(centers, radii,'EdgeColor','r');
-
-  %% Hough transform detection doesn't improve results
-
-  %% new approach: check with geometric information of border boxes if peach was detected twice
+  %% check with geometric information of border boxes if peach was detected twice
   measurements = peaches;
 
   f = fieldnames(measurements)';
@@ -25,7 +15,7 @@ function [peaches] = detectCenters(binary)
     addPeach = true;
 
     % look for bounding boxes of certain maximum size
-    if thisBB(3) <= 12 && thisBB(4) <= 12
+    if thisBB(3) <= 48 && thisBB(4) <= 48
       % if bounding box is small enough, look for near centroid in
       % already added data
       for i = 1: length(measurements)
@@ -34,7 +24,7 @@ function [peaches] = detectCenters(binary)
           distanceY = abs(measurements(k).Centroid(2) - measurements(i).Centroid(2));
           distance = sqrt(distanceX^2 + distanceY^2);
           % add only if no other center is far away enough
-          if distance < 20
+          if distance < 80
             addPeach = false;
           end
         end
@@ -47,18 +37,19 @@ function [peaches] = detectCenters(binary)
     end
   end
 
-  if ~isempty(peaches)
+  %% rescaling peach centers if they have been resized before
+  %if ~isempty(peaches)
     %% put center coordinates (x,y) in separate cell array and rescale them for tracking with sfm
-    elements = struct2cell(peaches')';
-    centers_scaled = elements(:,2);
-    centers = zeros(length(centers_scaled),2);
+  %  elements = struct2cell(peaches')';
+  %  centers_scaled = elements(:,2);
+  %  centers = zeros(length(centers_scaled),2);
 
-    for i = 1 : length(centers_scaled)
-      x_new = interp1([1 6000], [1 6000], centers_scaled{i}(1,1));
-      y_new = interp1([1 4000], [1 4000], centers_scaled{i}(1,2));
-      centers(i,:) = [x_new, y_new];
-    end
-  else
-    centers = [];
-  end
+  %  for i = 1 : length(centers_scaled)
+  %    x_new = interp1([1 6000*imgscale], [1 6000], centers_scaled{i}(1,1));
+  %    y_new = interp1([1 4000*imgscale], [1 4000], centers_scaled{i}(1,2));
+  %    centers(i,:) = [x_new, y_new];
+  %  end
+  %else
+  %  centers = [];
+  %end
 end
